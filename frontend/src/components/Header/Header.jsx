d681a63ba1d18ef852baa52248dee7d8d4013c0a
Héctor E. García <hegarciagg@gmail.com>
2023-11-26 00:04:39 -0500

    header_terminado

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,114 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import logo from "../../assets/Images/logo_2.png";
+import userImage from "../../assets/images/hector2.png";
+import { NavLink, Link } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
+
+const navLinks = [
+  {
+    name: "Inicio",
+    path: "/home",
+  },
+  {
+    name: "Login",
+    path: "/Login",
+  },
+  {
+    name: "Portafolio",
+    path: "/portfolio",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+];
 
 const Header = () => {
-  return <div>Header</div>;
+  /*Codigo hamburguesa*/
+
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+
+  const handleStickyHeader = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleStickyHeader();
+    return () => window.removeEventListener("scroll", handleStickyHeader);
+  });
+
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+
+  /*AQUI TERMINA EL CODIGO HAMBURGUESA*/
+  /* en el header coloco el código de referencia para el codigo hamburguesa */
+  return (
+    <header className="header flex items-center" ref={headerRef}>
+      <div className="container">
+        <div className="flex items-center justify-beetween">
+          {/*====LOGO====*/}
+          <div>
+            <img
+              className="w-32 h-32  rounded-full py-3 px-3"
+              src={logo}
+              alt="Logo Hector E Garcia"
+            />
+          </div>
+          {/*===MENU===*/}
+          <nav
+            className="navigation py-3 px-3"
+            ref={menuRef}
+            onClick={toggleMenu}
+          >
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={link.path}
+                    className={(navClass) =>
+                      navClass.isActive
+                        ? "colorBorder_reviews text-[16px] leading-7 font-[600] hover: colorPrincipal"
+                        : "colorText text-[16px] leading-7 font-[500] hover: colorPrincipal"
+                    }
+                  >
+                    {link.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          {/*===MENU DERECHA===*/}
+          <nav className="flex items-center gap-5 py-8 px-8">
+            <div>
+              <Link to="/">
+                <figure className="w-16 h-16 rounded-full cursor-pointer">
+                  <img src={userImage} className="w-full rounded-full" alt="" />
+                </figure>
+              </Link>
+            </div>
+
+            <Link to="/login">
+              <button className="bg-[#101220] py-2 px-6 w-[46px] h-[20px] rounded-full flex items-center justify-center">
+                Login
+              </button>
+            </Link>
+            <span className="md:hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,141 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import m_correlation from "../assets/Images/m-correlation.png";
+import frontera_eficiente from "../assets/Images/fronteraeficiente.png";
+import min_variance from "../assets/Images/min_varianza  .png";
+const stocks = [
+  "CIB",
+  "EC",
+  "GLD",
+  "GOOGL",
+  "IRBO",
+  "MSFT",
+  "QCOM",
+  "RTX",
+  "SCHH",
+  "SOXX",
+  "TSM",
+  "VNQ",
+  "VOO",
+];
 
-function Home() {
-  return <div>Home</div>;
+function SimulationPage() {
+  const [selectedStocks, setSelectedStocks] = useState([]);
+  const [customStock, setCustomStock] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numSimulations, setNumSimulations] = useState(1);
+  // Estado para los resultados (placeholder para el ejemplo)
+  const [results, setResults] = useState(null);
+
+  const handleCheckboxChange = (stock) => {
+    setSelectedStocks((prevSelectedStocks) =>
+      prevSelectedStocks.includes(stock)
+        ? prevSelectedStocks.filter((s) => s !== stock)
+        : [...prevSelectedStocks, stock]
+    );
+  };
+
+  const handleSimulate = () => {
+    // Lógica para realizar la simulación y generar resultados
+    // setResults(simulationResults);
+  };
+
+  return (
+    <div className="simulation-page flex flex-col md:flex-row">
+      <div className="md:w-1/2 p-5 space-y-6">
+        <h1 className="text-4xl font-bold mb-8">Simulador de Bolsa</h1>
+
+        <div className="stock-selection grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mb-6">
+          {stocks.map((stock) => (
+            <label
+              key={stock}
+              className="flex items-center space-x-2 cursor-pointer"
+            >
+              <input
+                type="checkbox"
+                checked={selectedStocks.includes(stock)}
+                className="form-checkbox rounded text-orange-500 focus:ring focus:ring-orange-300"
+                onChange={() => handleCheckboxChange(stock)}
+              />
+              <span className="text-gray-700">{stock}</span>
+            </label>
+          ))}
+        </div>
+
+        <div className="custom-stock mb-4">
+          <input
+            type="text"
+            value={customStock}
+            className="form-input w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
+            onChange={(e) => setCustomStock(e.target.value)}
+            placeholder="Adiciona otra acción"
+          />
+        </div>
+
+        <div className="date-selection flex space-x-4 mb-6">
+          <DatePicker
+            className="form-input w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+          <DatePicker
+            className="form-input w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+          />
+        </div>
+
+        <div className="simulation-inputs mb-6">
+          <input
+            type="number"
+            className="form-input w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-600"
+            value={numSimulations}
+            onChange={(e) => setNumSimulations(e.target.value)}
+            placeholder="Número de simulaciones"
+          />
+        </div>
+
+        <button
+          onClick={handleSimulate}
+          className="w-full md:w-auto btn bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700 transition-colors px-6 py-3"
+        >
+          Simular
+        </button>
+      </div>
+      <div className="md:w-1/2 p-5 space-y-6">
+        <h2 className="text-xl font-semibold text-center">
+          Resultados simulación{" "}
+        </h2>
+        <img src={m_correlation} alt="" className="rounded-lg shadow-xl" />
+        <h2 className="text-xl font-semibold text-center">
+          Frontera Eficiente
+        </h2>
+        <img
+          src={frontera_eficiente}
+          alt=""
+          className=" px-10 w-3/5 rounded-lg shadow-xl"
+        />
+        <h2 className="text-xl font-semibold text-center">
+          Portafolios Eficientes
+        </h2>
+        <img
+          src={min_variance}
+          alt=""
+          className=" px-20 rounded-lg shadow-xl"
+        />
+      </div>
+      <div className="results mt-8">
+        {/* Componentes o elementos para mostrar los resultados de la simulación */}
+        {results && (
+          <>
+            {/* Componentes para visualizar la matriz de correlación y la frontera eficiente */}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Home;
+export default SimulationPage;
